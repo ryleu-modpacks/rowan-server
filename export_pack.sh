ec9d0e556a77999994c05db1b8a182ed394f69fe
ryleu <69326171+ryleu@users.noreply.github.com>
2024-09-27 21:44:37 -0500

    test run

---
+++ export_pack.sh
@@ -0,0 +1,4 @@
+#!/bin/env bash
+
+cd resource_pack
+zip -r '/var/home/ryleu/.var/app/org.prismlauncher.PrismLauncher/data/PrismLauncher/instances/Fabulously Optimized(1)/.minecraft/resourcepacks/test.zip' .
